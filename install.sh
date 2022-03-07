64bd8e913cface3f0995b96e2edf92300230fd4f
FelipeFa6 <andres-escobar@protonmail.com>
2022-03-06 23:30:01 -0300

    Added UEFI install branch

--- install.sh
+++ install.sh
@@ -1,61 +1,43 @@
-DRIVE="/dev/sda";
-SYSTEMPARTITION="/dev/sda1";
-
-# Choose your drivers
-DRIVERS="AMD";
-#DRIVERS="INTEL";
-#DRIVERS="NVIDIA";
-
+# == MY ARCH SETUP INSTALLER == #
 #part1
-echo "Arch install script\n";
+printf '\033c'
+echo "Welcome to bugswriter's arch installer script"
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring
+loadkeys us
 timedatectl set-ntp true
 lsblk
-
-echo "Installation will be performed in the $DRIVE drive"
-read -p "Is this correct? [y/n]" answer
-if [[ $answer = n ]] ; then
-  lsblk
-  echo "Select the desired drive (/dev/sdX)"
-  read DRIVE
-fi
-cfdisk $DRIVE
-
-echo "Arch Linux will be installed in $SYSTEMPARTITION partition"
-read -p "Is this correct? [y/n]" answer
-if [[ $answer = n ]] ; then
-  lsblk
-  echo "Enter the base system partition (/dev/sdX1)"
-  read SYSTEMPARTITION
-fi
-mkfs.ext4 $SYSTEMPARTITION
-
-read -p "Did you create a SWAP partition? [y/n]" answer
+echo "Enter the drive: "
+read drive
+cfdisk $drive 
+echo "Enter the linux partition: "
+read partition
+mkfs.ext4 $partition 
+read -p "Did you also create efi partition? [y/n]" answer
 if [[ $answer = y ]] ; then
-  lsblk
-  echo "Enter the SWAP partition: "
-  read swapPartition
-  mkswap $swapPartition
-  swapon $swapPartition
+  echo "Enter EFI partition: "
+  read efipartition
+  mkfs.vfat -F 32 $efipartition
 fi
-
-mount $SYSTEMPARTITION /mnt
-pacstrap /mnt base base-devel linux-lts linux-firmware
+mount $partition /mnt 
+pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 sed '1,/^#part2$/d' `basename $0` > /mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
 exit 
 
-
 #part2
 printf '\033c'
-ln -sf /usr/share/zoneinfo/Chile/Continental /etc/localtime
+pacman -S --noconfirm sed
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+ln -sf /usr/share/zoneinfo/Chile/continental /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "Enter your Hostname: "
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "Hostname: "
 read hostname
 echo $hostname > /etc/hostname
 echo "127.0.0.1       localhost" >> /etc/hosts
@@ -63,40 +45,31 @@ echo "::1             localhost" >> /etc/hosts
 echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
 mkinitcpio -P
 passwd
+pacman --noconfirm -S grub efibootmgr os-prober
+echo "Enter EFI partition: " 
+read efipartition
+mkdir /boot/efi
+mount $efipartition /boot/efi 
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+sed -i 's/quiet/pci=noaer/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman --noconfirm -S grub
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg 
 
 pacman -S --noconfirm vim git networkmanager \
   xorg-server xorg-xinit libxinerama libxft webkit2gtk xorg-xsetroot xwallpaper \
-  gcr gstreamer gst-plugins-good gst-libav gst-plugins-base xdg-utils \
   pipewire pipewire-pulse pulsemixer \
   bluez bluez-utils \
   zsh doas zathura zathura-pdf-poppler sxiv inetutils
 
-#Video Drivers
-echo "Installing $DRIVERS Drivers"
-case $DRIVERS in
-        "AMD")
-					pacman -S --noconfirm xf86-video-amdgpu mesa
-					;;
-        "INTEL")
-					pacman -S --noconfirm xf86-video-intel mesa
-          ;;
-        "NVIDIA")
-					pacman -S --noconfirm nvidia nvidia-utils
-          ;;
-esac
-
-pacman -Rs --noconfirm sudo
+systemctl enable NetworkManager.service 
+rm /bin/sh
+ln -s dash /bin/sh
 echo "permit nopass :wheel" >> /etc/doas.conf
-systemctl enable NetworkManager
-echo "Enter a username: "
+echo "Enter Username: "
 read username
-useradd -mG wheel $username
+useradd -m -G wheel -s /bin/zsh $username
 passwd $username
-doas chsh -s $(which zsh) $username
 echo "Pre-Installation Finish Reboot now"
 ai3_path=/home/$username/arch_install3.sh
 sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
@@ -108,7 +81,6 @@ exit
 #part3
 printf '\033c'
 cd $HOME
-
 git clone --depth=1 https://github.com/felipefa6/.vim
 git clone --depth=1 https://github.com/felipefa6/dotfiles.git $HOME/.dotfiles
 cp -r $HOME/.dotfiles/.config $HOME/
@@ -129,5 +101,9 @@ doas make -C $installdir/dmenu install
 
 git clone --depth=1 https://git.suckless.org/surf $installdir/surf
 doas make -C $installdir/surf install
+
+cd $HOME
+rm -rf .bash*
+
 exit
 
