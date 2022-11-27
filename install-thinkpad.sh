e8323b81950948f66737cc258efe50e20d474940
FelipeFa6 <felipe.farias@coolebra.com>
2022-11-27 08:26:18 -0300

    Fixed thinkpad install

--- install-thinkpad.sh
+++ install-thinkpad.sh
@@ -78,44 +78,4 @@ su -c $ai3_path -s /bin/sh $username
 exit 
 
 #part3
-printf '\033c'
-cd $HOME
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/bugswriter/dotfiles.git tmpdotfiles
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-rm -r tmpdotfiles
-# dwm: Window Manager
-git clone --depth=1 https://github.com/Bugswriter/dwm.git ~/.local/src/dwm
-sudo make -C ~/.local/src/dwm install
-
-# st: Terminal
-git clone --depth=1 https://github.com/Bugswriter/st.git ~/.local/src/st
-sudo make -C ~/.local/src/st install
-
-# dmenu: Program Menu
-git clone --depth=1 https://github.com/Bugswriter/dmenu.git ~/.local/src/dmenu
-sudo make -C ~/.local/src/dmenu install
-
-# dmenu: Dmenu based Password Prompt
-git clone --depth=1 https://github.com/ritze/pinentry-dmenu.git ~/.local/src/pinentry-dmenu
-sudo make -C ~/.local/src/pinentry-dmenu clean install
-
-# dwmblocks: Status bar for dwm
-git clone --depth=1 https://github.com/bugswriter/dwmblocks.git ~/.local/src/dwmblocks
-sudo make -C ~/.local/src/dwmblocks install
-
-# pikaur: AUR helper
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
-makepkg -fsri
-cd
-pikaur -S libxft-bgra-git yt-dlp-drop-in
-mkdir dl dox imp music pix pub code
-
-ln -s ~/.config/x11/xinitrc .xinitrc
-ln -s ~/.config/shell/profile .zprofile
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv ~/.oh-my-zsh ~/.config/zsh/oh-my-zsh
-rm ~/.zshrc ~/.zsh_history
-alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dots config --local status.showUntrackedFiles no
 exit
